73506b1aea63870e4c5d3ccdd85656ce5f2fc27f
Yamushev Igor <iy@trinitydigital.ru>
2018-09-28 10:27:31 +0300

    Add window insets to stub layout

--- src/main/kotlin/space.alcorb/kex/stubs/StubLayout.kt
+++ src/main/kotlin/space.alcorb/kex/stubs/StubLayout.kt
@@ -3,10 +3,13 @@ package space.alcorb.kex.stubs
 import android.content.Context
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsets
 import android.widget.FrameLayout
 import space.alcorb.kex.android.views.hide
 
@@ -43,6 +46,12 @@ class StubLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet
         if (contentView == null)
             contentView = child
         super.addView(child, index, params)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT_WATCH)
+    override fun onApplyWindowInsets(insets: WindowInsets): WindowInsets {
+        for (i in 0 until childCount) getChildAt(i).dispatchApplyWindowInsets(insets)
+        return insets
     }
     
     fun isCurrentLoading(): Boolean =
